2300220f7c3a5ebdec16f6974f281d018930eb30
Fahri MacBook <fahri-can@web.de>
2019-05-30 03:01:21 +0200

    Create model for endpoint.

---
+++ app/src/main/java/com/example/rxjavajsonplaceholderapi/model/Post.kt
@@ -0,0 +1,8 @@
+package com.example.rxjavajsonplaceholderapi.model
+
+data class Post(
+    val userId: Int,
+    val id: Int,
+    val title: String,
+    val body: String
+)
--- app/src/main/java/com/example/rxjavajsonplaceholderapi/ui/MainActivity.kt
+++ app/src/main/java/com/example/rxjavajsonplaceholderapi/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.rxjavajsonplaceholderapi
+package com.example.rxjavajsonplaceholderapi.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rxjavajsonplaceholderapi.R
 
 class MainActivity : AppCompatActivity() {
 
