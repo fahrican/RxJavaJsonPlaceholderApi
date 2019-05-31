52de6abbf9ea8203d28257a5e7c49de57846254e
Fahri MacBook <fahri-can@web.de>
2019-05-31 19:22:32 +0200

    Add layout for post viewholder.

---
+++ app/src/main/java/com/example/rxjavajsonplaceholderapi/viewholder/PostViewHolder.kt
@@ -0,0 +1,7 @@
+package com.example.rxjavajsonplaceholderapi.viewholder
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+}
