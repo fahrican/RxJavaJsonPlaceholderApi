95e790ef83b0bbe3527b2a13e1326eecdf85257a
Fahri MacBook <fahri-can@web.de>
2019-05-31 19:26:24 +0200

    Finish viewholder for post.

--- app/src/main/java/com/example/rxjavajsonplaceholderapi/viewholder/PostViewHolder.kt
+++ app/src/main/java/com/example/rxjavajsonplaceholderapi/viewholder/PostViewHolder.kt
@@ -2,6 +2,18 @@ package com.example.rxjavajsonplaceholderapi.viewholder
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.post_card.view.*
 
 class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private var title: TextView
+    private var body: TextView
+    private var textId: TextView
+
+    init {
+        title = itemView.post_title
+        body = itemView.post_body
+        textId = itemView.post_id
+    }
 }
